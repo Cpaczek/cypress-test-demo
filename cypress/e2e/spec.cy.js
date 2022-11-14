b65a5fa1bcde77011aa4225683d25a7d65d7fce3
Cameron Paczek <cpazek@gmail.com>
2022-11-14 01:55:36 -0800

    add cypress test demo

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,15 @@
 describe('empty spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:3000/');
+    cy.get('.text-2xl > a').click();
+    cy.get('input').click();
+    cy.get('input').type('test');
+    cy.get('button').click();
+    cy.get('form').submit();
+    // check that alert opened
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Form submitted');
+    });
+
   })
 })
---
+++ pages/test.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- Createing a test form with a promise to test cypress -->
+    <form @submit.prevent="submitForm">
+        <input type="text" v-model="name" />
+        <button type="submit">Submit</button>
+    </form>
+
+</template>
+<script setup>
+
+// submitForm function with a fake promise
+const submitForm = async () => {
+
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, 1000)
+    })
+    //alert
+    alert('Form submitted')
+}
+
+</script>
